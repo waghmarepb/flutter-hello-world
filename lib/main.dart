04cc3abb01ff6b84fa1dbcb7debba422e8a8530c
Pradip Waghmare <waghmarepb@outlook.com>
2020-09-25 17:15:26 +0530

    update in app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(DemoApp());
+// }
+
+void main() => runApp(DemoApp());
+
+class DemoApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hell o world'),
+    );
+  }
+}
